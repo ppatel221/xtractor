f7dce2ba3869c78f5d29f5b58b0df3c4a4e6bb51
Parth <ppatel221@myseneca.ca>
2017-12-07 08:07:20 -0500

    Modified the hash functions to read the file

--- fileInfo.py
+++ fileInfo.py
@@ -1,6 +1,7 @@
 import sys 
 import os
 import hashlib 
+from pathlib import Path
 
 
 def getName(path):
@@ -11,11 +12,13 @@ def getSize(file):
 	size = os.path.getSize(file)
 
 def getSHA1(data):
+	content = Path(data).read_text()
 	SHA1Hash = hashlib.sha1()
-	SHA1Hash.update(data.encode())
+	SHA1Hash.update(content.encode())
 	return SHA1Hash.hexdigest()
 
 def getMD5(data):
+  	content = Path(data).read_text()
   	MD5hash = hashlib.md5()
-  	MD5hash.update(data.encode())
+  	MD5hash.update(content.encode())
   	return MD5hash.hexdigest()
